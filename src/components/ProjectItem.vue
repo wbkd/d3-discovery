705420764d2f60e7c858236746e1381071f25efe
errorroutine <tine@webkid.io>
2017-12-04 16:34:58 +0100

    feat(projects): add images

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="project-item">
+    <div v-if="item.url !== '' ">
+      <img class="project-image" v-bind:src="path" />
+    </div>
     <p class="project-title">{{item.name}}</p>
     <p class="project-description">{{item.description}}</p>
     <p class="project-latest">{{item.lastUpdate}}</p>
@@ -12,9 +15,15 @@
 <script>
   export default {
     props: ['item'],
+    computed: {
+      path() {
+        return require(`../../images/${this.item.name}.jpeg`);
+      }
+    }
   };
 </script>
 
 <style scoped lang="stylus">
-
+  .project-image
+    max-width: 100%
 </style>
