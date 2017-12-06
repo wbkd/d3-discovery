ef77e9404c1eef35f6214f7699888c1b73ed399a
errorroutine <tine@webkid.io>
2017-12-06 11:04:25 +0100

    fix(app): normalize body

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,17 @@ export default {
 };
 </script>
 
-<style scoped lang='stylus'>
-body
-  margin: 0
-  padding: 0
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
 
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
