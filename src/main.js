6b6600552271beea898a4cc90db2227651c72371
Stefan Keil <an.stefan.keil@googlemail.com>
2018-01-10 16:01:46 +0100

    update(Dropdown) fix select styles in ff

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueLazyload from 'vue-lazyload';
+
 import App from './App';
 import router from './router';
-import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false;
 
