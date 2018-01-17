f88a2c1f5234c4f572100bdbc9adda89054d9373
Stefan Keil <an.stefan.keil@googlemail.com>
2018-01-17 14:14:31 +0100

    update(Background) remove animation

--- src/components/BackgroundItems.vue
+++ src/components/BackgroundItems.vue
@@ -3,14 +3,12 @@
     <div class="background__gradient"/>
     <div
       class="background__item"
-      v-for="index in 10"
+      v-for="index in 20"
       :key="index"
       v-bind:style="{
         top: `${randomNumber(0, 100)}%`,
         left: `${randomNumber(0, 100)}%`,
-        transform: `scale(${randomNumber(0, 1.5)})`,
-        animationDelay: `${randomNumber(0, 5)}s`,
-        animationDuration: `${randomNumber(4, 15)}s`,
+        transform: `scale(${randomNumber(0, 2)})`
       }"
     />
   </div>
@@ -27,9 +25,6 @@
 </script>
 
 <style scoped lang="stylus">
-  random(min, max)
-	  (math(0, 'random')*(max - min + 1) + min)
-
   .main__background
     position: fixed
     background-image: linear-gradient(-180deg, #3C3863 3%, #0A081E 96%)
@@ -44,21 +39,10 @@
 
   .background__item
     z-index: 101
-    will-change: transform
     position: fixed
     width: 1px
     height: 1px
     background-color: rgba(255, 255, 255, .80)
 
     box-shadow: 0px 0px 3px 1px rgba(255, 255, 255, .40)
-    animation: animStar 5s ease-in infinite
-    transition: transform random(0, 3s) ease-in-out
-
-  @keyframes animStar
-    0%
-      transform: scale(1)
-    50%
-      transform: scale(random(1, 2))
-    100%
-      transform: scale(random(1, 3))
 </style>
