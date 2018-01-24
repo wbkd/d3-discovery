b5058062ff2f7b7c8287095f8126e069298f0224
Stefan Keil <an.stefan.keil@googlemail.com>
2018-01-24 14:05:13 +0100

    fix(Project Item) remove flickering of the teaser image

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -81,7 +81,6 @@
     background: #fff
     text-align: left
     border-radius: 2px
-    overflow: hidden
     font-size: 14px
     transition: box-shadow .3s ease-in-out
     box-shadow: 0 1px 3px 0 rgba(255,255,255,0.16)
