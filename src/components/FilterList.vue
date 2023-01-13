7f504c63e5c50c584973250f3e0e0b31e412519d
moklick <info@moritzklack.com>
2023-01-13 18:01:47 +0100

    fix(app): remove contributor stuff

--- src/components/FilterList.vue
+++ src/components/FilterList.vue
@@ -3,7 +3,7 @@
     <div class="filterList__inner">
       <Search class="filterList__item" :update="this.onSearchChange" />
 
-       <Slider
+      <Slider
         class="filterList__item"
         :slider-title="'Stars:'"
         :slider-value="stars.value"
@@ -19,109 +19,102 @@
         :slider-update="this.sliderContributorUpdate"
       />
 
-      <Dropdown class="filterList__item"
+      <Dropdown
+        class="filterList__item"
         :dropdown-title="'Latest Update:'"
         :value="this.activeUpdateFilter"
         :placeholder="'Any time'"
         :options="this.latestUpdateOptions"
         :on-change="this.onSelectLatestUpdate"
       />
-      <Dropdown class="filterList__item"
+      <Dropdown
+        class="filterList__item"
         :dropdown-title="'Category:'"
         :value="this.activeUpdateFilter"
         :placeholder="'All Categories'"
         :options="this.categoryOptions"
         :on-change="this.onCategoryFilterChanged"
       />
-      <Dropdown class="filterList__item"
+      <Dropdown
+        class="filterList__item"
         :dropdown-title="'License'"
         :value="this.activeLicenseFilter"
         :placeholder="'Any license'"
         :options="this.licenseOptions"
         :on-change="this.onLicenseFilterChanged"
       />
-
     </div>
   </div>
 </template>
 
 <script>
-  import VueSlider from 'vue-slider-component';
-  import Slider from './Slider';
-  import Search from '../components/Search';
-  import Dropdown from '../components/Dropdown';
+import VueSlider from "vue-slider-component";
+import Slider from "./Slider";
+import Search from "../components/Search";
+import Dropdown from "../components/Dropdown";
 
-  export default {
-    props: {
-      sliderContributorUpdate: Function,
-      sliderContributorValue: Array,
-      sliderStarsUpdate: Function,
-      sliderStarsValue: Array,
-      onSearchChange: Function,
-      update: Function,
-      menuOpen: Boolean,
-      data: Array,
-      onSelectLatestUpdate: Function,
-      activeUpdateFilter: String,
-      latestUpdateOptions: Array,
-      onLicenseFilterChanged: Function,
-      licenseOptions: Array,
-      activeLicenseFilter: String,
-      onCategoryFilterChanged: Function,
-      categoryOptions: Array,
-      activeCategoryFilter: String,
-    },
-    components: {
-      VueSlider,
-      Slider,
-      Search,
-      Dropdown,
-    },
-    data() {
-      return {
-        contributor: {
-          value: this.sliderContributorValue,
-          options: {
-            min: this.sliderContributorValue[0],
-            max: this.sliderContributorValue[1],
-          },
-        },
-        stars: {
-          value: this.sliderStarsValue,
-          options: {
-            min: this.sliderStarsValue[0],
-            max: this.sliderStarsValue[1],
-          },
-        },
-        width: 'auto',
-      };
-    },
-  };
+export default {
+  props: {
+    sliderStarsUpdate: Function,
+    sliderStarsValue: Array,
+    onSearchChange: Function,
+    update: Function,
+    menuOpen: Boolean,
+    data: Array,
+    onSelectLatestUpdate: Function,
+    activeUpdateFilter: String,
+    latestUpdateOptions: Array,
+    onLicenseFilterChanged: Function,
+    licenseOptions: Array,
+    activeLicenseFilter: String,
+    onCategoryFilterChanged: Function,
+    categoryOptions: Array,
+    activeCategoryFilter: String
+  },
+  components: {
+    VueSlider,
+    Slider,
+    Search,
+    Dropdown
+  },
+  data() {
+    return {
+      stars: {
+        value: this.sliderStarsValue,
+        options: {
+          min: this.sliderStarsValue[0],
+          max: this.sliderStarsValue[1]
+        }
+      },
+      width: "auto"
+    };
+  }
+};
 </script>
 
 <style scoped lang="stylus">
-  .filterList
-    position: relative
-    z-index: 10
+.filterList
+  position: relative
+  z-index: 10
 
+.filterList__inner
+  padding: 0
+
+.filterList__item
+  margin: 1em 0
+
+@media screen and (min-width: 414px)
   .filterList__inner
-    padding: 0
+    padding: 1em 0
+    display: grid
+    grid-template-columns: repeat(2, 1fr)
+    grid-gap: 1em 2em
+    align-items: center
 
   .filterList__item
-    margin: 1em 0
+    margin: 0
 
-  @media screen and (min-width: 414px)
-    .filterList__inner
-      padding: 1em 0
-      display: grid
-      grid-template-columns: repeat(2, 1fr)
-      grid-gap: 1em 2em
-      align-items: center
-
-    .filterList__item
-      margin: 0
-
-  @media screen and (min-width: 768px)
-    .filterList__inner
-      grid-template-columns: repeat(4, 1fr)
+@media screen and (min-width: 768px)
+  .filterList__inner
+    grid-template-columns: repeat(4, 1fr)
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,9 +7,7 @@
         :is-modal-visible="isModalVisible"
         :on-submit="showModal"
       />
-      <FilterList v-if="sliderContributorValue.length"
-        :slider-contributor-update="onSlideContributor"
-        :slider-contributor-value="sliderContributorValue"
+      <FilterList
         :slider-stars-update="onSlideStars"
         :slider-stars-value="sliderStarsValue"
         :update="onCheck"
@@ -27,13 +25,33 @@
       />
       <div class="content__info container">
         <div class="info__sort">
-          <div class="sort__label">Sort by: </div>
-          <SortButton :label="'Name'" :handler="this.onSortBy" :sort-key="'name'" v-bind:is-active="this.activeSortKey === 'name'" v-bind:sortAsc="this.sortAsc" />
-          <SortButton :label="'Stars'" :handler="this.onSortBy" :sort-key="'stars'" v-bind:is-active="this.activeSortKey === 'stars'" v-bind:sortAsc="this.sortAsc" />
-          <SortButton :label="'Latest Update'" :handler="this.onSortBy" :sort-key="'lastUpdate'" v-bind:is-active="this.activeSortKey === 'lastUpdate'" v-bind:sortAsc="this.sortAsc" />
+          <div class="sort__label">Sort by:</div>
+          <SortButton
+            :label="'Name'"
+            :handler="this.onSortBy"
+            :sort-key="'name'"
+            v-bind:is-active="this.activeSortKey === 'name'"
+            v-bind:sortAsc="this.sortAsc"
+          />
+          <SortButton
+            :label="'Stars'"
+            :handler="this.onSortBy"
+            :sort-key="'stars'"
+            v-bind:is-active="this.activeSortKey === 'stars'"
+            v-bind:sortAsc="this.sortAsc"
+          />
+          <SortButton
+            :label="'Latest Update'"
+            :handler="this.onSortBy"
+            :sort-key="'lastUpdate'"
+            v-bind:is-active="this.activeSortKey === 'lastUpdate'"
+            v-bind:sortAsc="this.sortAsc"
+          />
         </div>
 
-        <div class="info__search">{{filteredProjects.length || 0}} plugins found</div>
+        <div class="info__search">
+          {{ filteredProjects.length || 0 }} plugins found
+        </div>
       </div>
 
       <ProjectList :projects="filteredProjects" />
@@ -44,242 +62,260 @@
         no plugins found
       </div>
     </div>
-    <footer class="footer">Made by <a href="https://webkid.io" target="_blank">webkid</a></footer>
-
+    <footer class="footer">
+      Made by <a href="https://webkid.io" target="_blank">webkid</a>
+    </footer>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
+import axios from "axios";
 
-  import FilterList from '../components/FilterList';
-  import MainHeader from '../components/MainHeader';
-  import ProjectList from '../components/ProjectList';
-  import Modal from '../components/Modal';
-  import SortButton from '../components/SortButton';
+import FilterList from "../components/FilterList";
+import MainHeader from "../components/MainHeader";
+import ProjectList from "../components/ProjectList";
+import Modal from "../components/Modal";
+import SortButton from "../components/SortButton";
 
-  import { inRange, byDate, searchBy, uniqueByKey } from '../utils/filter';
-  import { capitalizeFirstLetter } from '../utils/strings';
+import { inRange, byDate, searchBy, uniqueByKey } from "../utils/filter";
+import { capitalizeFirstLetter } from "../utils/strings";
 
-  export default {
-    components: {
-      FilterList,
-      ProjectList,
-      MainHeader,
-      Modal,
-      SortButton,
-    },
-    data() {
-      return {
-        isModalVisible: false,
-        projects: [],
-        search: '',
-        sliderContributorValue: [],
-        sliderStarsValue: [],
-        latestUpdateFilterList: {
-          'Last week': 604800000,
-          'Last month': 2628000000,
-          'Last year': 31536000000,
-        }, // in ms
-        activeUpdateFilter: '',
-        licenseFilters: [],
-        activeLicenseFilter: '',
-        sortAsc: false,
-        activeSortKey: 'name',
-        categoryFilters: [],
-        activeCategoryFilter: '',
-      };
-    },
-    mounted() {
-      axios.get('https://d3-discovery-api.netlify.app/projects.json')
-        .then((response) => {
-          this.projects = response.data;
+export default {
+  components: {
+    FilterList,
+    ProjectList,
+    MainHeader,
+    Modal,
+    SortButton
+  },
+  data() {
+    return {
+      isModalVisible: false,
+      projects: [],
+      search: "",
+      sliderStarsValue: [],
+      latestUpdateFilterList: {
+        "Last week": 604800000,
+        "Last month": 2628000000,
+        "Last year": 31536000000
+      }, // in ms
+      activeUpdateFilter: "",
+      licenseFilters: [],
+      activeLicenseFilter: "",
+      sortAsc: false,
+      activeSortKey: "name",
+      categoryFilters: [],
+      activeCategoryFilter: ""
+    };
+  },
+  mounted() {
+    axios
+      .get("https://d3-discovery-api.netlify.app/projects.json")
+      .then(response => {
+        this.projects = response.data;
 
-          this.sliderContributorValue = [Math.min(...this.projects.map(d => d.contributors)), 100];
-          this.sliderStarsValue = [Math.min(...this.projects.map(d => d.stars)), 10000];
+        this.sliderStarsValue = [
+          Math.min(...this.projects.map(d => d.stars)),
+          10000
+        ];
 
-          this.licenseFilters = uniqueByKey(this.projects, 'license')
-            .sort((a, b) => a.localeCompare(b));
+        this.licenseFilters = uniqueByKey(
+          this.projects,
+          "license"
+        ).sort((a, b) => a.localeCompare(b));
 
-          this.categoryFilters = uniqueByKey(this.projects, 'category')
-            .map(d => capitalizeFirstLetter(d))
-            .sort((a, b) => a.localeCompare(b));
+        this.categoryFilters = uniqueByKey(this.projects, "category")
+          .map(d => capitalizeFirstLetter(d))
+          .sort((a, b) => a.localeCompare(b));
 
-          this.onSortBy(this.activeSortKey, 'string');
-        })
-        .catch(error => console.log(error));
-    },
-    computed: {
-      filteredProjects() {
-        const now = Date.now();
+        this.onSortBy(this.activeSortKey, "string");
+      })
+      .catch(error => console.log(error));
+  },
+  computed: {
+    filteredProjects() {
+      const now = Date.now();
 
-        return this.projects
-          .filter(project =>
+      return this.projects
+        .filter(
+          project =>
             !this.search ||
-              searchBy(
-                this.search,
-                [project.description, project.githubDescription, project.name, project.license],
-              ),
+            searchBy(this.search, [
+              project.description,
+              project.githubDescription,
+              project.name,
+              project.license
+            ])
+        )
+        .filter(project =>
+          inRange(
+            project.stars,
+            this.sliderStarsValue[0],
+            this.sliderStarsValue[1]
           )
-          .filter(project =>
-            inRange(
-              project.stars,
-              this.sliderStarsValue[0],
-              this.sliderStarsValue[1],
-            ),
-          )
-          .filter(project =>
-            inRange(
-              project.contributors,
-              this.sliderContributorValue[0],
-              this.sliderContributorValue[1],
-            ),
-          )
-          .filter((project) => {
-            const update = project.lastUpdate ? project.lastUpdate.trim() : null;
-            return !this.activeUpdateFilter ||
-             byDate(now, update, this.latestUpdateFilterList[this.activeUpdateFilter.trim()]);
-          })
-          .filter((project) => {
-            const license = project.license ? project.license.trim() : null;
-            return !this.activeLicenseFilter || license === this.activeLicenseFilter.trim();
-          })
-          .filter((project) => {
-            const category = project.category ? project.category.trim() : null;
-            return !this.activeCategoryFilter || (category || '').toLowerCase() === (this.activeCategoryFilter.trim() || '').toLowerCase();
-          });
-      },
-    },
-    methods: {
-      onSortBy(sortKey) {
-        if (this.activeSortKey === sortKey) {
-          this.sortAsc = !this.sortAsc;
-        }
+        )
+        .filter(project => {
+          const update = project.lastUpdate ? project.lastUpdate.trim() : null;
+          return (
+            !this.activeUpdateFilter ||
+            byDate(
+              now,
+              update,
+              this.latestUpdateFilterList[this.activeUpdateFilter.trim()]
+            )
+          );
+        })
+        .filter(project => {
+          const license = project.license ? project.license.trim() : null;
+          return (
+            !this.activeLicenseFilter ||
+            license === this.activeLicenseFilter.trim()
+          );
+        })
+        .filter(project => {
+          const category = project.category ? project.category.trim() : null;
+          return (
+            !this.activeCategoryFilter ||
+            (category || "").toLowerCase() ===
+              (this.activeCategoryFilter.trim() || "").toLowerCase()
+          );
+        });
+    }
+  },
+  methods: {
+    onSortBy(sortKey) {
+      if (this.activeSortKey === sortKey) {
+        this.sortAsc = !this.sortAsc;
+      }
 
-        this.activeSortKey = sortKey;
-        switch (sortKey) {
-          case 'name':
-            this.projects.sort((a, b) =>
-              (this.sortAsc ?
-                a[sortKey].localeCompare(b[sortKey]) : b[sortKey].localeCompare(a[sortKey])));
-            break;
-          case 'stars':
-            this.projects.sort((a, b) =>
-              (this.sortAsc ? b.stars - a.stars : a.stars - b.stars));
-            break;
-          case 'lastUpdate':
-            this.projects.sort((a, b) => {
-              const aToDateObj = new Date(a.lastUpdate);
-              const bToDateObj = new Date(b.lastUpdate);
-              return (this.sortAsc ? bToDateObj - aToDateObj : aToDateObj - bToDateObj);
-            });
-            break;
-          default:
-            this.projects.sort((a, b) =>
-              (this.sortAsc ?
-                a[sortKey].localeCompare(b[sortKey]) : b[sortKey].localeCompare(a[sortKey])));
-            break;
-        }
-      },
-      onSearchChange(input) {
-        this.search = input;
-      },
-      onCheck(input) {
-        this.checkLicense = input;
-      },
-      onSlideContributor(value) {
-        this.sliderContributorValue = value;
-      },
-      onSlideStars(value) {
-        this.sliderStarsValue = value;
-      },
-      onSelectLatestUpdate(event) {
-        this.activeUpdateFilter = event.target.value;
-      },
-      onSelectLicense(event) {
-        this.activeLicenseFilter = event.target.value;
-      },
-      onSelectCategory(event) {
-        this.activeCategoryFilter = event.target.value;
-      },
-      showModal() {
-        this.isModalVisible = true;
-      },
-      closeModal() {
-        this.isModalVisible = false;
-      },
+      this.activeSortKey = sortKey;
+      switch (sortKey) {
+        case "name":
+          this.projects.sort((a, b) =>
+            this.sortAsc
+              ? a[sortKey].localeCompare(b[sortKey])
+              : b[sortKey].localeCompare(a[sortKey])
+          );
+          break;
+        case "stars":
+          this.projects.sort((a, b) =>
+            this.sortAsc ? b.stars - a.stars : a.stars - b.stars
+          );
+          break;
+        case "lastUpdate":
+          this.projects.sort((a, b) => {
+            const aToDateObj = new Date(a.lastUpdate);
+            const bToDateObj = new Date(b.lastUpdate);
+            return this.sortAsc
+              ? bToDateObj - aToDateObj
+              : aToDateObj - bToDateObj;
+          });
+          break;
+        default:
+          this.projects.sort((a, b) =>
+            this.sortAsc
+              ? a[sortKey].localeCompare(b[sortKey])
+              : b[sortKey].localeCompare(a[sortKey])
+          );
+          break;
+      }
     },
-  };
+    onSearchChange(input) {
+      this.search = input;
+    },
+    onCheck(input) {
+      this.checkLicense = input;
+    },
+    onSlideStars(value) {
+      this.sliderStarsValue = value;
+    },
+    onSelectLatestUpdate(event) {
+      this.activeUpdateFilter = event.target.value;
+    },
+    onSelectLicense(event) {
+      this.activeLicenseFilter = event.target.value;
+    },
+    onSelectCategory(event) {
+      this.activeCategoryFilter = event.target.value;
+    },
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    }
+  }
+};
 </script>
 
-<style scoped lang='stylus'>
+<style scoped lang="stylus">
+.main
+  position relative
+  min-height: 100vh
+  overflow: hidden
+
+.main__content
+  position: relative
+  flex-grow: 1
+  margin-bottom: 5em
+
+.content__info
+  display: flex
+  justify-content: flex-start
+  flex-flow: row wrap
+  align-items center
+
+  color: #eee
+  margin-bottom: 2em
+  padding-left: 2em
+  font-size: 12px
+
+.info__sort
+  display: flex
+  align-items: center
+  flex-flow: row wrap
+  align-content: center
+  text-align: center
+
+.sort__label
+  margin-right: .3em
+  text-align: left
+
+.info__search
+  text-align: left
+  flex-grow: 1
+  font-size: 12px
+
+@media screen and (min-width: 498px)
+  .info__search
+    text-align: right
+
+@media screen and (min-width: 786px)
   .main
-    position relative
-    min-height: 100vh
-    overflow: hidden
-
-  .main__content
-    position: relative
-    flex-grow: 1
-    margin-bottom: 5em
-
-  .content__info
     display: flex
-    justify-content: flex-start
-    flex-flow: row wrap
-    align-items center
+    align-items: stretch
 
-    color: #eee
-    margin-bottom: 2em
-    padding-left: 2em
-    font-size: 12px
-
-  .info__sort
-    display: flex
-    align-items: center
-    flex-flow: row wrap
-    align-content: center
-    text-align: center
-
-  .sort__label
-    margin-right: .3em
-    text-align: left
+  .footer
+    position: absolute
+    bottom: 0
+    left: 0
+    width: 100%
+    padding: 1em
+    background-color: rgba(225,193,225,0.08)
+    color: white
+    a
+      color: white
 
   .info__search
-    text-align: left
-    flex-grow: 1
-    font-size: 12px
+  .content__info
+    font-size: 14px
 
-  @media screen and (min-width: 498px)
-    .info__search
-      text-align: right
+  .content__info
+    padding-left: 1em
 
-  @media screen and (min-width: 786px)
-    .main
-      display: flex
-      align-items: stretch
+  .info__sort
+    justify-content: flex-start
 
-    .footer
-      position: absolute
-      bottom: 0
-      left: 0
-      width: 100%
-      padding: 1em
-      background-color: rgba(225,193,225,0.08)
-      color: white
-      a
-        color: white
-
-    .info__search
-    .content__info
-      font-size: 14px
-
-    .content__info
-      padding-left: 1em
-
-    .info__sort
-      justify-content: flex-start
-
-    .sort__label
-      margin-right: .5em
+  .sort__label
+    margin-right: .5em
 </style>
